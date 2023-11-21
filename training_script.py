74c857728bc8ff7c933e75fd7b7314d0109afccb
Thomas Ambrogini <thomas@MacBook-Pro-di-Thomas.local>
2023-11-21 15:30:30 +0100

    training script for the model

--- training_script.py
+++ training_script.py
@@ -24,34 +24,33 @@ if __name__ == "__main__":
     parser.add_argument('--epochs', type=int, default=os.environ.get('SM_OUTPUT_DATA_DIR'))
     parser.add_argument('--model-dir', type=str,  default=10)
     args, _ = parser.parse_known_args()
-
-		class_count = 100
+    class_count = 100
 
     data_path = '/opt/ml/input/data/train/'
-		with np.load(data_path + 'dataset.npz') as data:
-		        train_faces = data["arr_0"]
-		        train_labels = data["arr_1"]
+    with np.load(data_path + 'dataset.npz') as data:
+	train_faces = data["arr_0"]
+        train_labels = data["arr_1"]
 
-		face_images = preprocess_input(np.array(train_faces))
+    face_images = preprocess_input(np.array(train_faces))
 		
-		out_encode = LabelEncoder()
-		out_encode.fit(train_labels)
-		face_labels = out_encode.transform(train_labels)
+    out_encode = LabelEncoder()
+    out_encode.fit(train_labels)
+    face_labels = out_encode.transform(train_labels)
 
-		x_train, x_test, y_train, y_test = train_test_split(face_images, face_labels, train_size=0.8, stratify=face_labels, random_state=0)
+    x_train, x_test, y_train, y_test = train_test_split(face_images, face_labels, train_size=0.8, stratify=face_labels)
 
-		base_model = ResNet50(weights='imagenet', include_top=False)
-		base_model.trainable = False
+    base_model = ResNet50(weights='imagenet', include_top=False)
+    base_model.trainable = False
+	
+    model = Sequential()
+    model.add(Resizing(224, 224))
+    model.add(base_model)
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dense(class_count, activation='softmax'))
 		
-		model = Sequential()
-		model.add(Resizing(224, 224))
-		model.add(base_model)
-		model.add(Flatten())
-		model.add(Dense(1024, activation='relu'))
-		model.add(Dense(class_count, activation='softmax'))
-		
-		model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-		hist = model.fit(x_train, y_train, validation_data=(x_test, y_test), batch_size=10, epochs=10)
+    hist = model.fit(x_train, y_train, validation_data=(x_test, y_test), batch_size=10, epochs=epochs)
 
     model.save('/opt/ml/model/actor-model/2')
